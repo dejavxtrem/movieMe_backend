2c9eef25ae85a09d3e23a84a2f9ddf1017fe76f2
dejavxtrem <dejavxtrem@hotmail.com>
2020-05-19 00:56:15 -0500

    backend construction

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,12 +1,3 @@
-const mongoose = require('mongoose');
-const movieUsers = mongoose.Schema({
-    name: { type: String,  required: true},
-    email: { type: String, required: true, lowercase: true, unique: true},
-    password: String
-},{timestamp: true})
-
-
-module.exports = mongoose.model('movieuserdata', movieUsers);
 
 
 
---
+++ models/movieusers.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const movieUsers = mongoose.Schema({
+    name: { type: String,  required: true},
+    email: { type: String, required: true, lowercase: true, unique: true},
+    password: String
+},{timestamp: true})
+
+
+module.exports = mongoose.model('User', movieUsers);
